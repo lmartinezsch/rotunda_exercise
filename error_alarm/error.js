c299c796a4c49b63f5ac17c161ef628ff8a015ea
Leandro Martinez <leandro.martinez@thelucky.app>
2024-03-12 07:56:53 -0300

    fix problem with error_alarm

--- error_alarm/error.js
+++ error_alarm/error.js
@@ -1,29 +1,31 @@
 const ONE_MINUTE_IN_MILLISECONDS = 60000;
 const MAX_ERROR_COUNT = 10;
 
-let errorCountSinceLastNotificacion = 0;
+let lastErrorsBuffer = [];
 let lastNotificationDate = null;
 
 async function handleError(error) {
     await logError(error);
-    errorCountSinceLastNotificacion++;
 
-    if (errorCountSinceLastNotificacion > MAX_ERROR_COUNT && (new Date() - lastNotificationDate) > ONE_MINUTE_IN_MILLISECONDS) {
+    let errorCountInTheLastMinute = lastErrorsBuffer.filter(error => error.date > (new Date() - ONE_MINUTE_IN_MILLISECONDS)).length;
+
+    if (errorCountInTheLastMinute > MAX_ERROR_COUNT && (new Date() - lastNotificationDate) > ONE_MINUTE_IN_MILLISECONDS) {
         await sendNotification();
     }
 }
 
-async function sendNotification() {
-    lastNotificationDate = new Date();
-    errorCountSinceLastNotificacion = 0;
-    console.log('Notification sent', lastNotificationDate);
+async function logError(error) {
+    lastErrorsBuffer.push({date: new Date(), error})
+    lastErrorsBuffer = lastErrorsBuffer.slice(-(MAX_ERROR_COUNT + 1)); // Keep only the last MAX_ERROR_COUNT + 1 errors
+    console.log("Error saved:", error);
 }
 
-async function logError(error) {
-    console.log("Error saved", error);
+async function sendNotification() {
+    lastNotificationDate = new Date();
+    console.log('Notification sent at', lastNotificationDate);
 }
 
 // Test the functionality
 setInterval(async () => {
-    await handleError(`Error`);
+    await handleError('An error has occurred');
 }, 1000, 0)
